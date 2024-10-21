1095f89be8368eaefc86c973d2232a8fe5dcc5be
mhdnihas <muhammednihas8589@gmail.com>
2024-10-21 16:56:54 +0530

    created streamlit app

--- app.py
+++ app.py
@@ -29,3 +29,30 @@ def read_sql_query(sql,db):
 
     return row
 
+
+prompt=["""
+        Your are the expert in converting English question to SQL query !
+        The SQL Database has the student table and which have following columns -NAME , CLASS , SECTION , MARKS .\n \n 
+        For example , example 1 - How many entries of records are present ? , the SQL query something like this select * from student ; 
+        example 2 - who are studying Mearnstack ? the SQL query something like this select * from student where CLASS="Mern Stack";
+        """]
+
+st.set_page_config(page_title="I can Retrieve Any SQL query")
+
+st.header("Gemini App To Retrieve SQL Data")
+
+question = st.text_input("Input:",key='input')
+
+submit = st.button("Ask the question")
+
+
+if submit:
+
+    sql_query=get_gemini_response(question=question,prompt=prompt)
+    print("SQL qeurty:",sql_query)
+    response=read_sql_query(sql=sql_query,db="student.db")
+    
+    st.subheader("Response is ...")
+    for row in response:
+        print(row)
+        st.header(row)
